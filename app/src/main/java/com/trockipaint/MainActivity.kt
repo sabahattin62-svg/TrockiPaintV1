ceccc023c9d667bdc078ac8f4ab57c361a5d7fee
sabahattin62-svg <sabahattin62@gmail.com>
2026-08-16 20:30:55 +0300

    Update MainActivity.kt

--- app/src/main/java/com/trockipaint/MainActivity.kt
+++ app/src/main/java/com/trockipaint/MainActivity.kt
@@ -1,1 +1,17 @@
+pack
 package com.trockipaint
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("TrockiPaint V1")
+        }
+    }
+}
