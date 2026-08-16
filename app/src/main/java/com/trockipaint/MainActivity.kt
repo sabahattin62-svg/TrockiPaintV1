f0a31ae27b24ec1a4e76ddb2b7b86443f84d26cf
sabahattin62-svg <sabahattin62@gmail.com>
2026-08-17 01:30:42 +0300

    Update MainActivity.kt

--- app/src/main/java/com/trockipaint/MainActivity.kt
+++ app/src/main/java/com/trockipaint/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.trockipaint
 
+import android.app.Activity
 import android.os.Bundle
+import android.graphics.Color
+import android.view.Gravity
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val textView = TextView(this)
-        textView.text = "TrockiPaint V1"
+        textView.text = "TroçkiPaint V1\nÇalışıyor!"
         textView.textSize = 28f
+        textView.setTextColor(Color.BLACK)
+        textView.setBackgroundColor(Color.WHITE)
+        textView.gravity = Gravity.CENTER
 
         setContentView(textView)
     }
