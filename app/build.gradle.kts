2d361d354f383e21421a7c0887865984ccac30dc
sabahattin62-svg <sabahattin62@gmail.com>
2026-08-16 22:28:06 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,16 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    
+}    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
